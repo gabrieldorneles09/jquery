d22f777cd9c0a204cd72238846c2cec218a63616
Gabriel Dorneles <gabrieldorneles09@gmail.com>
2018-05-23 12:58:28 -0300

    Conclusao

--- alura-typer/public/js/placar.js
+++ alura-typer/public/js/placar.js
@@ -3,7 +3,7 @@ $("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Gabriel"
+    var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
@@ -76,6 +76,13 @@ function sincronizaPlacar(){
 
         $.post(baseUrl+"placar", dados, () => {
             console.log("Placar sincronizado com sucesso");
+            $(".tooltip").tooltipster("open");
+        }).fail(function(){
+            $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar");
+        }).always(function(){
+            setTimeout(function(){
+                $(".tooltip").tooltipster("close");
+            }, 1200);
         });
     });
   
